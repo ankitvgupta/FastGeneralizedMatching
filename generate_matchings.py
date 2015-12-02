e2b98afe1ca59b9b87948d893bc506fcdd169666
ANKIT GUPTA <ankitgupta@college.harvard.edu>
2015-12-02 00:08:44 -0500

    made generate matchings only generate doctor preferences for doctors that apply to hospital

--- generate_matchings.py
+++ generate_matchings.py
@@ -1,24 +1,41 @@
 # Randomly generates a set of matchings for patients and for doctors.
-
 import numpy as np
+import csv
 
-
-numDoctors = 500
-numHospitals = 50
+numDoctors = 30
+numHospitals = 3
 numPositionsPerHospital = 8
-numPreferencesPerDoctor = 20
+numPreferencesPerDoctor = 2
 
 doctor_prefs = []
+hospital_applications = {}
+
+# Start by initializing empty arrays for each hospital
+for hospital in xrange(numHospitals):
+	hospital_applications[hospital] = []
+
+# Determine up to numPreferencesPerDoctor hospital preferences per doctor
+# Submit an "application" to each of those hospitals
 for doctor in xrange(numDoctors):
     preferences = np.random.permutation(numHospitals)[:numPreferencesPerDoctor]
+    # add each preffed hospital to that hospital's applications
+    for preference in preferences:
+    	hospital_applications[preference].append(doctor)
     doctor_prefs.append(preferences)
+# Write to file
 doctor_prefs = np.array(doctor_prefs).astype(int)
 np.savetxt("doctor_preferences.txt", doctor_prefs, fmt="%5d")
 hospital_prefs = []
-for hospital in xrange(numHospitals):
-    preferences = np.random.permutation(numDoctors)
-    hospital_prefs.append(preferences)
-hospital_prefs = np.array(hospital_prefs).astype(int)
-np.savetxt("hospital_preferences.txt", hospital_prefs, fmt="%5d")
+
+# For each received application, give them some order, and write to file
+for hospital, applications in zip(hospital_applications.keys(), hospital_applications.values()) :
+    preferences = np.random.permutation(applications)
+    hospital_prefs.append(preferences.astype(int))
+print hospital_prefs
+with open('hospital_preferences.txt', 'wb') as csvfile:
+	writer = csv.writer(csvfile, delimiter=' ')
+	for pref in hospital_prefs:
+		writer.writerow(pref)
+
 
 
